cccc5a091106628568cf65492b4609b8e0e752ce
Coder <coder@dev.lan>
2019-08-10 16:49:22 +0000

    start over

--- src/Effects.hs
+++
@@ -1,9 +0,0 @@
-module Effects (
-    module Effects.Actor,
-    module Effects.Communicator,
-    module Effects.Logger
-) where
-
-import Effects.Actor
-import Effects.Communicator
-import Effects.Logger
