8b568508cabd4de945c2d7a19fda23caa8209e78
Jinnah Ali-Clarke <ja@Jinnahs-MacBook-Pro.local>
2019-08-16 09:53:47 -0400

    server connector

--- app/Actors.hs
+++ app/Actors.hs
@@ -1,10 +1,11 @@
 module Actors (
-    socketWriterManager
+    socketWriterManager,
+    serverConnector
 ) where
 
 import qualified System.IO as IO
 import Data.ByteString.Lazy (ByteString, hPut)
-import Network.Socket (Socket)
+import qualified Network.Socket as N
 import Network.Socket.ByteString.Lazy (sendAll)
 
 import Actor
@@ -16,12 +17,21 @@ fileWriter filePath =
         IO.hSetBuffering handle IO.NoBuffering
         actorTerminal (hPut handle)
 
-socketWriter :: Socket -> IO (Mailbox IO ByteString)
+socketWriter :: N.Socket -> IO (Mailbox IO ByteString)
 socketWriter socket = actorTerminal (sendAll socket)
 
-socketWriterManager :: Socket -> FilePath -> IO (Mailbox IO ByteString)
+socketWriterManager :: N.Socket -> FilePath -> IO (Mailbox IO ByteString)
 socketWriterManager socket logFilePath =
     do
         loggerMailbox <- fileWriter logFilePath
         socketMailbox <- socketWriter socket
         forward [loggerMailbox, socketMailbox]
+
+serverConnector :: Mailbox IO N.Socket -> IO (Mailbox IO (String, String))
+serverConnector mailbox =
+    actor [mailbox] $ \(host, port) -> do
+        let hints = N.defaultHints {N.addrSocketType = N.Stream}
+        addr : _ <- N.getAddrInfo (Just hints) (Just host) (Just port)
+        socket <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
+        N.connect socket (N.addrAddress addr)
+        pure socket
