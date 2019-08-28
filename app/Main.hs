82ddba29f638513f46b0e162aa2caa643985adc0
Coder <coder@dev.lan>
2019-08-28 23:18:12 +0000

    done rework! need to now kill actors when not needed

--- app/Main.hs
+++ app/Main.hs
@@ -1,56 +1,69 @@
--- import qualified Data.ByteString as B
--- import qualified Network.Socket as N
--- import qualified Network.Socket.ByteString as NB
--- import qualified System.IO as IO
--- import System.Environment (getArgs)
+import qualified Control.Monad.IO.Class as MTL
+import Data.Functor (void)
+import qualified Data.ByteString as B
+import qualified Network.Socket as N
+import qualified Network.Socket.ByteString as NB
+import System.Environment (getArgs)
+import qualified System.IO as IO
 
--- import Actor
 
--- fileWriter :: FilePath -> IO (Mailbox IO B.ByteString)
--- fileWriter filePath = do
---     handle <- IO.openBinaryFile filePath IO.AppendMode
---     IO.hSetBuffering handle IO.NoBuffering
---     registerTerminal (B.hPut handle)
+import Actor
+import ActorIO
 
--- connectionIdentifier :: N.SockAddr -> FilePath
--- connectionIdentifier = const "connection.log"
+fileWriter :: FilePath -> ActorIO (Mailbox ActorIO B.ByteString)
+fileWriter filePath =
+    let initializer = MTL.liftIO $ do
+            handle <- IO.openBinaryFile filePath IO.AppendMode
+            IO.hSetBuffering handle IO.NoBuffering
+            pure handle
+    in registerTerminal initializer (\handle -> MTL.liftIO . B.hPut handle) (MTL.liftIO . IO.hClose)
 
--- socketWriter :: N.Socket -> IO (Mailbox IO B.ByteString)
--- socketWriter socket = registerTerminal (NB.sendAll socket)
+connectionIdentifier :: N.SockAddr -> FilePath
+connectionIdentifier = const "connection.log"
 
--- socketReader :: N.Socket -> [Mailbox IO B.ByteString] -> IO ()
--- socketReader socket = registerSource (NB.recv socket 1024)
+socketWriter :: N.Socket -> ActorIO (Mailbox ActorIO B.ByteString)
+socketWriter socket = registerTerminal (pure socket) (\sock -> MTL.liftIO . NB.sendAll sock) (MTL.liftIO . N.close)
 
--- actorCreator :: String -> String -> IO (Mailbox IO (N.Socket, N.SockAddr))
--- actorCreator host port = registerTerminal $ \(acceptedSocket, clientAddrInfo) -> do
---     let identifier = connectionIdentifier clientAddrInfo
---         hints = N.defaultHints {N.addrSocketType=N.Stream}
+socketReader :: N.Socket -> [Mailbox ActorIO B.ByteString] -> ActorIO ()
+socketReader socket = registerSource (pure socket) (\sock -> MTL.liftIO $ NB.recv sock 1024) (MTL.liftIO . N.close)
 
---     serverAddrInfo : _ <- N.getAddrInfo (Just hints) (Just host) (Just port)
---     serverSocket <- N.socket (N.addrFamily serverAddrInfo) (N.addrSocketType serverAddrInfo) (N.addrProtocol serverAddrInfo)
---     N.connect serverSocket (N.addrAddress serverAddrInfo)
+actorCreatorAction :: String -> String -> (N.Socket, N.SockAddr) -> ActorIO ()
+actorCreatorAction host port (acceptedSocket, clientAddrInfo) = do
+    let identifier = connectionIdentifier clientAddrInfo
+        hints = N.defaultHints {N.addrSocketType=N.Stream}
 
---     clientWriterMailbox <- socketWriter acceptedSocket
---     clientRequestLoggerMailbox <- fileWriter ("client_" ++ identifier)
---     serverWriterMailbox <- socketWriter serverSocket
---     serverResponseLoggerMailbox <- fileWriter ("server_" ++ identifier)
+    serverSocket <- MTL.liftIO $ do
+        serverAddrInfo : _ <- N.getAddrInfo (Just hints) (Just host) (Just port)
+        serverSocket <- N.socket (N.addrFamily serverAddrInfo) (N.addrSocketType serverAddrInfo) (N.addrProtocol serverAddrInfo)
+        N.connect serverSocket (N.addrAddress serverAddrInfo)
+        pure serverSocket
 
---     socketReader acceptedSocket [clientRequestLoggerMailbox, serverWriterMailbox]
---     socketReader serverSocket [serverResponseLoggerMailbox, clientWriterMailbox]
+    clientWriterMailbox <- socketWriter acceptedSocket
+    clientRequestLoggerMailbox <- fileWriter ("client_" ++ identifier)
+    serverWriterMailbox <- socketWriter serverSocket
+    serverResponseLoggerMailbox <- fileWriter ("server_" ++ identifier)
 
--- main :: IO ()
--- main = do
---     [listenPort, host, port] <- getArgs
+    socketReader acceptedSocket [clientRequestLoggerMailbox, serverWriterMailbox]
+    socketReader serverSocket [serverResponseLoggerMailbox, clientWriterMailbox]
 
---     let hints = N.defaultHints {N.addrSocketType=N.Stream, N.addrFlags=[N.AI_PASSIVE]}
---     addrInfo : _ <- N.getAddrInfo (Just hints) (Nothing) (Just listenPort)
---     socket <- N.socket (N.addrFamily addrInfo) (N.addrSocketType addrInfo) (N.addrProtocol addrInfo)
---     N.setSocketOption socket N.ReuseAddr 1
---     N.bind socket (N.addrAddress addrInfo)
---     N.listen socket 5
-
---     actorCreatorMailbox <- actorCreator host port
---     registerSourceTopLevel (N.accept socket) [actorCreatorMailbox]
+actorCreator :: String -> String -> ActorIO (Mailbox ActorIO (N.Socket, N.SockAddr))
+actorCreator host port = registerTerminal (pure ()) (const $ actorCreatorAction host port) (const $ pure ())
 
 main :: IO ()
-main = pure ()
+main =
+    let initializer listenPort = MTL.liftIO $ do
+            let hints = N.defaultHints {N.addrSocketType=N.Stream, N.addrFlags=[N.AI_PASSIVE]}
+            addrInfo : _ <- N.getAddrInfo (Just hints) (Nothing) (Just listenPort)
+            socket <- N.socket (N.addrFamily addrInfo) (N.addrSocketType addrInfo) (N.addrProtocol addrInfo)
+            N.setSocketOption socket N.ReuseAddr 1
+            N.bind socket (N.addrAddress addrInfo)
+            N.listen socket 5
+            pure socket
+    in void . runActorIO $ do
+        args <- MTL.liftIO getArgs
+        case args of
+            [listenPort, host, port] -> do
+                actorCreatorMailbox <- actorCreator host port
+                registerTopLevelSource (initializer listenPort) (MTL.liftIO . N.accept) (MTL.liftIO . N.close) [actorCreatorMailbox]
+            _ -> MTL.liftIO $ putStrLn "usage: mitm listenPort host port"
+
